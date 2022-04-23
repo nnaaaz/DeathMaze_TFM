fca22cbe5ca76b099963d4e300e14f375c80d454
Pshy0 <Pshy0@users.noreply.github.com>
2022-04-23 03:57:58 +0200

    fixed function name

--- lua/nnaaaz_death_maze.lua
+++ lua/nnaaaz_death_maze.lua
@@ -29,7 +29,7 @@ tfm.exec.disablePhysicalConsumables(true)
 tfm.exec.disableMinimalistMode(true)
 tfm.exec.disableDebugCommand(true)
 tfm.exec.disableAutoScore(true)
-system.disableChatCommandsDisplay(nil, true)
+system.disableChatCommandDisplay(nil, true)
 
 
 
